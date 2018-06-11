c66a2807befe7846bd98bde242814f8375e1966b
Nikita Kasyanov <nik.kasyanov@gmail.com>
2018-06-11 20:09:52 +0300

    created media recorder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MemoList from "./MemoList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      memos: [
+        {
+          date: new Date(),
+          audio: null,
+          text: ""
+        },
+        {
+          date: new Date(),
+          audio: null,
+          text: ""
+        },
+        {
+          date: new Date(),
+          audio: null,
+          text: ""
+        },
+        {
+          date: new Date(),
+          audio: null,
+          text: ""
+        }
+      ]
+    };
+
+    this.state.mediaSupported = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+
+    if (this.state.mediaSupported) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          this.mediaRecorder = new MediaRecorder(stream);
+        })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">memo</h1>
+        <header className="App__header">
+          <h1 className="App__title">memo</h1>
         </header>
-        <div className="App-content">
-          Memo list will go here
-        </div>
+        {this.state.mediaSupported ?
+          (<div className="App__content">
+            <MemoList memos={this.state.memos}/>
+          </div>) :
+          (<div className="App__error">Media input is not supported by browser</div>)
+        }
       </div>
     );
   }
