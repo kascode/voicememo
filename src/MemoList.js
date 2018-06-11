43436428825d51ee1f53d12eb18ff7ec87171124
Nikita Kasyanov <nik.kasyanov@gmail.com>
2018-06-11 20:09:40 +0300

    render memo list

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import 'MemoList.css';
+import React from "react";
+import Memo from "./Memo";
+import "./MemoList.css";
 
-const MemoList = ({memos}) => {
-
+const MemoList = ({ memos }) => {
+  return (
+    <div className="MemoList">
+      {memos.map(memo => {
+        return <Memo data={memo} />;
+      })}
+    </div>
+  );
 };
 
 export default MemoList;
