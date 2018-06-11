45ba556596fa8aad56430b73fbff1260c2ac24d3
Nikita Kasyanov <nik.kasyanov@gmail.com>
2018-06-11 20:09:30 +0300

    memo exampledisplay

--- src/Memo.js
+++ src/Memo.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import 'Memo.css';
+import React from "react";
+import "./Memo.css";
 
-const Memo = ({memo}) => {
-
+const Memo = ({ data }) => {
+  return (
+    <div className="Memo">
+      <div className="Memo__title">{data.date.getUTCDate()}</div>
+      <div className="Memo__content">
+        Test text in place of real message
+      </div>
+    </div>
+  );
 };
 
 export default Memo;
